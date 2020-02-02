0650ceeff2644ceb2f9b4ff9f3ec28ac07aad4ae
Chapin Bryce <27cbryce@gmail.com>
2020-02-02 12:42:17 -0500

    Incremented version

--- libchickadee/__init__.py
+++ libchickadee/__init__.py
@@ -5,6 +5,6 @@ Yet another GeoIP resolution tool.
 
 __author__ = 'Chapin Bryce'
 __date__ = 20200107
-__version__ = 20200202
+__version__ = 20200202.1
 __license__ = 'MIT Copyright 2020 Chapin Bryce'
 __desc__ = '''Yet another GeoIP resolution tool.'''
