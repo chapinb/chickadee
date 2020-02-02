04d1c2c95b43b3a9a73eef30ae99e8cf81da9176
Chapin Bryce <27cbryce@gmail.com>
2020-02-02 10:25:11 -0500

    Updated test runner and coverage reporting

--- push_checks.sh
+++ push_checks.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 fgrep -Ri pdb libchickadee/**/*.py
 flake8 libchickadee --count --show-source --statistics
-coverage run tests.py && coverage html
-open htmlcov/index.html
+coverage run -m unittest discover
+coverage xml
+coverage report
